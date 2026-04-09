3c567c3d4aeb1135953023a5b46f2d86f3c7ed6b
shubhangi23-tach <mishrashubhangi407@gmail.com>
2026-04-09 22:23:10 +0530

    First DevOps Task

---
+++ app.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Hello! Mera pehla DevOps task chal raha hai!');
+});
+server.listen(3000, () => console.log('Server running on port 3000'));
